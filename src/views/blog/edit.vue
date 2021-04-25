5827a2199b92327aab35ee3814a6517caf949c02
18263759936 <1435585893@qq.com>
2021-04-25 14:59:11 +0800

    [view] edit页面功能完善

--- src/views/blog/edit.vue
+++ src/views/blog/edit.vue
@@ -2,36 +2,29 @@
 	<div>
 		<!-- 博客信息 -->
 		<el-row>
-			<el-col :span="12">
+			<el-col :span="8">
+				<!-- 标题 -->
 				<el-row>
 					<span>标题：</span>
 					<el-input class="input-normal" size="small" v-model="title"></el-input>
 				</el-row>
 
+				<!-- 标签 -->
 				<el-row class="margin-top tag">
 					<span>标签：</span>
-					<el-select
-					class="input-normal"
-					size="small"
-					v-model="keyword"
-					multiple
-					filterable
-					allow-create
-					default-first-option
-					placeholder="请选择文章标签">
-						<el-option
-						v-for="item in options"
-						:key="item.value"
-						:label="item.value"
-						:value="item.value">
-						</el-option>
-					</el-select>
+					<tags-input placeholder="请输入标签" :keyword="keyword" :options="options" @change="changeTags"></tags-input>
 				</el-row>
 			</el-col>
 
-			<el-col :span="12" class="textarea">
-				<div class="span-label">描述：</div>
-				<el-input type="textarea" size="small" class="textarea-input" :rows="3" v-model="des"></el-input>
+			<!-- 描述 -->
+			<el-col :span="8" class="textarea">
+					<div class="span-label">描述：</div>
+					<el-input type="textarea" size="small" class="textarea-input" :rows="4" v-model="des"></el-input>
+			</el-col>
+
+			<!-- 上传 -->
+			<el-col :span="8">
+				<read-one-file accept=".txt,.md" @readText="readText"></read-one-file>
 			</el-col>
 		</el-row>
 		
@@ -40,7 +33,7 @@
 
 		<!-- 功能按钮 -->
 		<div class="bottom-button">
-			<el-button type="success" size="small" @click="save">保存</el-button>
+			<el-button type="success" size="small" @click="save" :disabled='dataChange'>保存</el-button>
 			<el-button type="warning" size="small" @click="back">返回</el-button>
 		</div>
 	</div>
@@ -50,8 +43,11 @@
 import Vditor from "vditor";
 import "vditor/dist/index.css";
 import { apiGetBlog , apiPostBlog , apiPutBlog } from '../../api/blog';
+import readOneFile from '../../components/readOneFile/readOneFile.vue';
+import TagsInput from '../../components/TagsInput/TagsInput.vue'
 
 export default {
+	components: { readOneFile , TagsInput },
 	data(){
 		return {
 			id: this.$route.params.id,	// 标志博客id，0时为新建博客
@@ -70,19 +66,31 @@ export default {
 			constDes: '',
 
 			/* 编辑器数据 */
-			content: '',			// 编辑器中的值
-			constContent: '',				// 原本博客中的值
+			content: '',			// 编辑器对象
+			constContent: '',		// 初始请求到的值
+			editChange: false		// 编辑器是否改变
+		}
+	},
+	computed: {
+		dataChange(){
+			// 判断数据是否发生了改变
+			return this.title == this.constTitle
+			&& this.keyword == this.constKeyword
+			&& this.des == this.constDes
+			&& !this.editChange;
 		}
 	},
 	mounted(){
 		this.init();
 	},
 	beforeRouteLeave(to, from, next){
-		if(to.path == '/login'){
+		// 离开页面提示
+		if(to.path == '/login' || to.query.save == 'true' || this.dataChange == true){
+			// 跳转首页时，设置query的save为true时，值未发生改变时；这三种情况直接跳转	
 			next();
 			return false;
 		}
-		this.$confirm(`确定离开编辑页面`, '提示', {
+		this.$confirm(`确定不保存离开编辑页面吗？`, '提示', {
 			confirmButtonText: '确定',
 			cancelButtonText: '取消',
 			type: 'warning'
@@ -96,6 +104,21 @@ export default {
 		async init(){
 			// 缩入菜单栏
 			this.$store.commit('setOpen',true);
+			await this.initEdit();
+		},
+
+		// 改变标签回调
+		changeTags(data){
+			this.keyword = data;
+		},
+		// 读取成功回调
+		readText(data){
+			this.content.setValue(data);
+			this.$message.success('读取成功');
+			this.checkEdit();
+		},
+		// 初始化Vditor编辑器
+		async initEdit(){
 			// 初始化编辑器
 			this.content = new Vditor("vditor",{
 				height: 320,		// 编辑器高度
@@ -106,25 +129,41 @@ export default {
 					enable:false
 				},
 				placeholder: 'Hello,please!',	// 提示
+				// 输入触发（延迟较大）
+				input: () => {
+					this.checkEdit();
+				},
+				blur: () => {
+					this.checkEdit();
+				},
 				after:async ()=>{
 					// 加载后执行的内容
 					if(this.id != 0){
 						// 如果不是新增则发送请求获取数据
-						let { dataInfo } = await apiGetBlog({id:this.id});
-						this.title = this.constTitle = dataInfo.title;
-						this.des = this.constDes = dataInfo.des;
-						this.keyword = this.constKeyword = dataInfo.keyword.split(',');
-						this.constContent = dataInfo.content;
-						this.content.setValue(dataInfo.content);
+						await this.initData();
 					}else{
 						this.content.setValue('');
 					}
 				}
 			});
 		},
+		// 检测编辑器内的值是否发生改变
+		checkEdit(){
+			if(this.constContent == this.content.getValue()){
+				// 值未改变
+				this.editChange = false;
+			}else{
+				// 值发生改变
+				this.editChange = true;
+			}
+		},
 
 		// 保存
 		async save(){
+			// 检查规则
+			if(this.checkFail()){
+				return false;
+			}
 			let params = {
 				author_id: this.$store.state.userInfo.userInfo.id,
 				author: this.$store.state.userInfo.userInfo.username,
@@ -136,17 +175,47 @@ export default {
 			if(this.id != 0){
 				// 修改模式
 				params.id = this.id;
-				console.log(params)
-				response = await apiPostBlog(params);
+				const res = await apiPostBlog(params);
+				if(res.data.flag == 0 && res.status == 250) {
+					// 更新失败
+					this.$message.error(res.data.msg)
+				}else{
+					// 更新成功
+					// location.reload();
+					this.initData();
+				}
 				return false;
 			}
 			// 新建模式
-			response = await apiPutBlog(params);
+			await apiPutBlog(params);
+			this.initData();
+			// location.reload();
 		},
 		// 返回
 		back(){
-			this.$router.replace('/manage/blog');
+			// 页面可能不进行跳转，需要用catch捕捉导航故障
+			this.$router.replace('/manage/blog').catch(err => {});
 		},
+
+		// 检测提交的数据是否符合规则
+		checkFail(){
+			if(this.title == ''){
+				this.$message.error('标题不能为空')
+				return true;
+			}
+		},
+
+		// 获取初始化数据,注：不能直接调用，需要再edit初始化后才可以调用
+		async initData(){
+			let { dataInfo } = await apiGetBlog({id:this.id});
+			this.title = this.constTitle = dataInfo.title;
+			this.des = this.constDes = dataInfo.des;
+			this.keyword = this.constKeyword = dataInfo.keyword.split(',');
+			this.constContent = dataInfo.content;
+			this.content.setValue(dataInfo.content);
+			// 将编辑器更改的标识符重置为未更改，即false
+			this.editChange = false;
+		}
 	}
 }
 </script>
@@ -154,8 +223,7 @@ export default {
 <style lang="less" scoped>
 // 标题输入框
 .input-normal{
-	width: 320px;
-	// margin-left: 10px;
+	width: 220px;
 }
 
 // 描述文本框
@@ -169,8 +237,7 @@ export default {
 		line-height: 32px;
 	}
 	.textarea-input{
-		width: 300px;
-		height: 100%;
+		width: 240px;
 	}
 }
 
