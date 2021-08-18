c6b07802031cfc5a7b19bd44f3a76a14a9987b3d
孙启萌 <hb.sunqimeng@aisino.com>
2021-08-18 11:36:12 +0800

    配置/学习vite打包

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import path from 'path';
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     createVuePlugin(),
   ],
