5c90b3e403ec184c10ff9ff78b13408ef154c88b
hb.sunqimeng <hb.sunqimeng@aisino.com>
2023-03-31 10:47:38 +0800

    build:修改生产环境请求基础地址

--- dist/assets/index.9bbe22d4.js
+++
@@ -1,1 +0,0 @@
-import{n as _}from"./index.0727ac14.js";import"./vendor.53200e5a.js";var a=function(){var e=this,n=e.$createElement,r=e._self._c||n;return r("div",[e._v(" appList ")])},s=[];const o={name:"appList"},t={};var i=_(o,a,s,!1,l,null,null,null);function l(e){for(let n in t)this[n]=t[n]}var u=function(){return i.exports}();export{u as default};
