6d4fc060992cee71b243264f0219bcdd6df52774
hb.sunqimeng <hb.sunqimeng@aisino.com>
2023-09-05 15:42:53 +0800

    publish

--- src/views/application/comapp/index.vue
+++ src/views/application/comapp/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="comapp-view">
     <!-- vite 子应用需要关闭沙箱 disableSandbox, 使用 inline 内联script模式 -->
     <micro-app
+      class="comapp"
       name="comapp"
       :url="url"
       disable-sandbox
@@ -27,6 +28,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.comapp-view{
+  height: 100%;
+  .comapp{
+    height: 100%;
+    position: relative;
+  }
+}
 
 </style>
