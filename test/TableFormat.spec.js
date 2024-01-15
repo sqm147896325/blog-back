afea9efc6cb5cda5ccafc19237c03f495c7ffa17
sunqimeng <1435585893@qq.com>
2024-01-15 22:50:35 +0800

    暂存

--- test/TableFormat.spec.js
+++ test/TableFormat.spec.js
@@ -1,16 +1,13 @@
 import { mount } from '@vue/test-utils'
-
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'element-plus/dist/index.css' // 导入全局样式
 
 import TableFormat from '../src/components/TableFormat/TableFormat.vue'
 
 describe('TableFormat 组件测试', () => {
-  it('组件挂载', () => {
-    const wrapper = mount(TableFormat, {
+  it('组件挂载', async () => {
+    const wrapper = await mount(TableFormat, {
       global: {
-        plugins: [ElementPlus, ElementPlusIconsVue]
+        plugins: [ElementPlus]
       },
       props: {
         modelValue: true,
@@ -21,39 +18,23 @@ describe('TableFormat 组件测试', () => {
       }
     })
 
-    expect(wrapper.element).toMatchSnapshot()
-  })
+    // const table = await wrapper.get('.cell')
 
-  it('确定方法', () => {
-    const wrapper = mount(TableFormat, {
-      global: {
-        plugins: [ElementPlus, ElementPlusIconsVue]
-      },
-      props: {
-        modelValue: true,
-        tableOption: [
-          { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 },
-          { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 }
-        ]
-      }
-    })
+    // console.log(table)
 
+    expect(wrapper.text()).toContain('格式')
+    expect(wrapper.get('.format-row').text()).toContain('确定', '重置')
     wrapper.vm.$nextTick(() => {
-      // 模拟点击确定按钮
-      wrapper.find('.format-row button:nth-child(1)').trigger('click')
-
-      // 检查是否触发了setTableOption事件
-      expect(wrapper.emitted('setTableOption')).toBeTruthy()
-
-      // 检查是否触发了update:modelValue事件
-      expect(wrapper.emitted('update:modelValue')).toBeTruthy()
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.text()).toContain('标题')
+      })
     })
   })
 
-  it('重置方法', () => {
-    const wrapper = mount(TableFormat, {
+  it('确定方法', async () => {
+    const wrapper = await mount(TableFormat, {
       global: {
-        plugins: [ElementPlus, ElementPlusIconsVue]
+        plugins: [ElementPlus]
       },
       props: {
         modelValue: true,
@@ -64,21 +45,50 @@ describe('TableFormat 组件测试', () => {
       }
     })
 
-    wrapper.vm.$nextTick(() => {
-      // 修改数据
-      wrapper.vm.tableData = [[
-        { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 },
-        { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 }
-      ]]
+    // 模拟点击确定按钮
+    await wrapper.get('.format-row button:nth-child(1)').trigger('click')
 
-      // 模拟点击重置按钮
-      wrapper.find('.format-row button:nth-child(2)').trigger('click')
+    // 检查是否触发了setTableOption事件
+    const setTableOption = wrapper.emitted('setTableOption')
+    expect(setTableOption).toBeTruthy()
+    // setTableOption 第一次触发 的 第一个返回值
+    expect(setTableOption[0][0]).toEqual([
+      { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 },
+      { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 }
+    ])
 
-      // 检查数据是否重置成功
-      expect(wrapper.vm.tableData).toEqual([
-        { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 },
-        { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 }
-      ])
+    // 检查是否触发了update:modelValue事件
+    expect(wrapper.emitted('update:modelValue')).toBeTruthy()
+  })
+
+  it('重置方法', async () => {
+    const wrapper = await mount(TableFormat, {
+      global: {
+        plugins: [ElementPlus]
+      },
+      props: {
+        modelValue: true,
+        tableOption: [
+          { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 },
+          { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 }
+        ]
+      }
     })
+
+    // // 修改数据
+    // await wrapper.setProps({
+    //   tableOption: [
+    //     { field: 'author', cname: '作者名', cshow: true, align: 'center', showname: '作者名', clock: false, width: 100 },
+    //     { field: 'title', cname: '标题', cshow: true, align: 'center', showname: '标题', clock: false, width: 180 }
+    //   ]
+    // })
+
+    // expect(wrapper.vm.tableData.map(e => e.cname)).toEqual(['作者名', '标题'])
+
+    // 模拟点击重置按钮
+    await wrapper.get('.format-row button:nth-child(2)').trigger('click')
+
+    // 检查数据是否重置成功
+    expect(wrapper.vm.tableData.map(e => e.cname)).toEqual(['标题', '作者名'])
   })
 })
